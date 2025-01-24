384fce0319dcfa8130273bc387c8732f7621a193
Jhonatan <pruebajhdx@gmail.com>
2025-01-24 09:02:12 -0500

    blogs

--- functions.php
+++ functions.php
@@ -1,2 +1,45 @@
 <?php
 add_theme_support('post-thumbnails');
+
+
+function load_blog_posts_ajax() {
+    $paged = isset($_POST['paged']) ? intval($_POST['paged']) : 1;
+
+    // Consulta personalizada para las publicaciones
+    $args = array(
+        'posts_per_page' => 8, // Cantidad de publicaciones por página
+        'paged'          => $paged
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post(); ?>
+            <div class="col-md-4">
+                <div class="item-blog">
+                    <div class="image-blog">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php if (has_post_thumbnail()) : ?>
+                                <?php the_post_thumbnail('full'); ?>
+                            <?php else : ?>
+                                <img src="<?php bloginfo('template_url') ?>/assets/img/default-image.png">
+                            <?php endif; ?>
+                        </a>
+                        <span><?php the_category(', '); ?></span>
+                    </div>
+                    <div class="text-blog">
+                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <p><?php echo wp_trim_words(get_the_excerpt(), 13, '...'); ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile;
+    else :
+        echo '<p>' . esc_html__('Lo siento, no hay más entradas.', 'text-domain') . '</p>';
+    endif;
+
+    wp_reset_postdata();
+    wp_die();
+}
+add_action('wp_ajax_load_blog_posts', 'load_blog_posts_ajax');
+add_action('wp_ajax_nopriv_load_blog_posts', 'load_blog_posts_ajax');
--- home.php
+++ home.php
@@ -11,8 +11,18 @@
         </div>
       </div>
     </div>
-    <div class="row">
-      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div class="row" id="blogPost">
+      <?php
+      // Configuración de la consulta personalizada
+      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1; // Obtiene el número de página actual
+      $args = array(
+        'posts_per_page' => 8, // Limitar a 1 publicación por página
+        'paged'          => $paged
+      );
+      $query = new WP_Query($args);
+
+      if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post(); ?>
           <div class="col-md-4">
             <div class="item-blog">
               <div class="image-blog">
@@ -34,30 +44,88 @@
         <?php endwhile;
       else : ?>
         <p><?php esc_html_e('Lo siento, no hay entradas disponibles.'); ?></p>
-      <?php endif; ?>
-
+      <?php endif;
+      $total_pages = $query->max_num_pages; ?>
     </div>
+
     <div class="row">
       <div class="col-md-6 offset-md-3">
         <div class="pagination-blog">
-          <ul>
-            <li> <a href=""><img src="<?php bloginfo('template_url') ?>/assets/img/dot-pagination.svg">
+          <ul class="pagination-directory">
+            <li>
+              <a href="" class="item qmenos">
+                <img src="<?php bloginfo('template_url') ?>/assets/img/dot-pagination.svg">
                 <p>Volver</p>
-              </a></li>
-            <li> <a href="">1</a></li>
-            <li> <a href="">2</a></li>
-            <li> <a class="active" href="">3</a></li>
-            <li> <a href="">4</a></li>
-            <li> <a href="">5</a></li>
-            <li> <a href="">
-                <p>Avanzar</p><img src="<?php bloginfo('template_url') ?>/assets/img/dot-pagination.svg">
-              </a></li>
+              </a>
+            </li>
+            <?php
+            // $total_pages = 5; // Reemplaza con el número total de páginas calculado dinámicamente.
+            for ($i = 1; $i <= $total_pages; $i++) {
+              if ($i == 1) {
+                echo '<li> <a href="" class="item active">' . $i . '</a></li>';
+              } else {
+                echo '<li> <a href="" class="item">' . $i . '</a></li>';
+              }
+            }
+            ?>
+            <li>
+              <a href="" class="item qmas">
+                <p>Avanzar</p>
+                <img src="<?php bloginfo('template_url') ?>/assets/img/dot-pagination.svg">
+              </a>
+            </li>
           </ul>
         </div>
       </div>
     </div>
   </div>
 </section>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+    jQuery(document).ready(function($) {
+        // Evento para manejar clics en los elementos de la paginación
+        $('.pagination-directory').on('click', '.item', function(e) {
+            e.preventDefault();
+
+            // Determinar la página solicitada
+            let paged;
+            if ($(this).hasClass('qmenos')) {
+                // Página anterior
+                paged = Number($('.pagination-directory .active').text()) - 1;
+            } else if ($(this).hasClass('qmas')) {
+                // Página siguiente
+                paged = Number($('.pagination-directory .active').text()) + 1;
+            } else {
+                // Página específica
+                paged = Number($(this).text());
+            }
+
+            // Prevenir que el número de página salga de los límites
+            if (paged < 1 || paged > <?php echo $total_pages; ?>) return;
+
+            // Actualizar la clase activa
+            $('.pagination-directory .item').removeClass('active');
+            $(`.pagination-directory .item:contains(${paged})`).addClass('active');
+            // Realizar solicitud AJAX
+            $.ajax({
+                url: '<?php echo admin_url("admin-ajax.php"); ?>',
+                type: 'post',
+                data: {
+                    action: 'load_blog_posts',
+                    paged: paged // Página solicitada
+                },
+                success: function(response) {
+                    // Actualizar el contenido del blog con la respuesta
+                    $('#blogPost').html(response);
+                },
+                error: function(error) {
+                    console.log('Error en la solicitud:', error);
+                }
+            });
+        });
+    });
+</script>
 
 <?php get_footer()
 ?>
